e12523223e6bd03a884647c57e113c77841b085f
Chris-CHA <numa4199@yahoo.com>
2020-04-30 08:50:36 -0400

    Large update to Customer Class
    Added Constructors and removed PRNG elements from Customer.java

--- Customer.java
+++ Customer.java
@@ -1,12 +1,17 @@
 /* Customer.java
  * Description: Customer Class for ISSM project
  * Status: Incomplete
- * Version: 2020.4.27-1
+ * Version: 2020.4.30-1
  * Authors: Christopher Herras-Antig, Dylan Ott
  */
+
+import java.util.*;
+
 public class Customer {
 
 	private float serveTime, balkChance, jockeyChance, renegeChance;
+	private String name;
+	private int numItems;
 	
 	public Customer()
 	{
@@ -16,16 +21,50 @@ public class Customer {
 		balkChance = 0.1f;
 		jockeyChance = 0.3f;
 		renegeChance = 0.5f;
-	}
-
-	
-	//PRNG METHOD
-	public int genRandom()
-	{
 		
-		return 0;
+		// Init
+		name = "John Doe";
+		numItems = 0;
 	}
 	
+	public Customer (String name) {
+		// set all fields to random #s within 0-1 (1-5 for serveTime)
+		// hardcoded values for TESTING ONLY -- REPLACE WITH PRNG VALUES LATER
+		serveTime = 2.1f;
+		balkChance = 0.1f;
+		jockeyChance = 0.3f;
+		renegeChance = 0.5f;
+		
+		// Init
+		name = "John Doe";
+		numItems = 0;
+	}
+	
+	public Customer (String name, int numItems) {
+		// set all fields to random #s within 0-1 (1-5 for serveTime)
+		// hardcoded values for TESTING ONLY -- REPLACE WITH PRNG VALUES LATER
+		serveTime = 2.1f;
+		balkChance = 0.1f;
+		jockeyChance = 0.3f;
+		renegeChance = 0.5f;
+		
+		// Init
+		this.numItems = numItems;
+		this.name = name;
+	}	
+
+	public Customer (String name, int numItems, float serveTime, float balkChance, float jockeyChance, float renegeChance) {
+		// set all fields to random #s within 0-1 (1-5 for serveTime)
+		// hardcoded values for TESTING ONLY -- REPLACE WITH PRNG VALUES LATER
+		this.serveTime = serveTime;
+		this.balkChance = balkChance;
+		this.jockeyChance = jockeyChance;
+		this.renegeChance = renegeChance;
+		
+		// Init
+		this.numItems = numItems;
+		this.name = name;
+	}	
 	
 	// stub code
 	public float getServeTime() {
