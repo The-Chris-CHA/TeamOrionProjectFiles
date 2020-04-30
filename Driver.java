bb9efab3d35a4c162bdbeacdfce767546d05a047
Chris-CHA <numa4199@yahoo.com>
2020-04-30 14:24:49 -0400

    Major Driver Update
    Reconfigured several variables and cleaned up unused variables.
    
    Created several methods for simulation.

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 /* Driver.java
  * Description: Driver class for ISSM project.
  * Status: Incomplete
- * Version: 2020.4.30-1
+ * Version: 2020.4.30-3
  * Authors: Christopher Herras-Antig, Dylan Ott, Name 3, Name 4
  */
 
@@ -10,28 +10,28 @@ import java.util.Random;
 
 public class Driver {
 	
+	// Declare all essentials
 	static BufferedReader input = new BufferedReader (new InputStreamReader(System.in));
+	
 	static int numServers, balkThreshold;
-	static float projRenegeChance;
+	static float projRenegeChance, totalTime;
 	static String inputStr, inputVal;
-	static Random rng = new Random();
 	
 	static Server server1 = new Server();
 	static Server server2 = new Server();
 	
+	static EventLogArchive events = new EventLogArchive();
+	
+	static Random rng = new Random(); // To Be Removed
+	
+	// Begin Subprograms
 	public static void main(String[] args) throws IOException {
 		// Init vars
 		boolean exitLoop = false;
+		totalTime = 0.0f; // This will be one of the most important vars
 		numServers = 1;
 		projRenegeChance = 0.75f; // Static chance for testing.
 		balkThreshold = 5; // Static chance for testing.
-		//projRenegeChance = giveRandomChance();
-		
-		// Debug
-		//System.out.println(rng.nextInt(2));
-		//System.out.println(projBalkChance);
-		//System.out.println(projRenegeChance);
-		//System.out.println(projJockeyChance);
 		
 		// Bootup Message
 		System.out.println("Welcome to the SSM Queue and Server System!"
@@ -41,8 +41,8 @@ public class Driver {
 		
 		// Menu Inputs
 		// Separated on different print functions for easy editing
-		System.out.println("1. Create a [Customer/Item]");
-		System.out.println("2. Process a [Customer/Item]");
+		System.out.println("1. Create a Customer");
+		System.out.println("2. Process a Customer");
 		System.out.println("3. Open the second server");
 		System.out.println("4. Close the second server");
 		System.out.println("5. Find q-hat");
@@ -61,11 +61,11 @@ public class Driver {
 			
 			switch (inputStr) {
 			case "1":
-				// Code for entering Create method
+				createCustomer();
 				break;
 				
 			case "2":
-				// Code for entering process method
+				processSingleCustomer();
 				break;
 				
 			case "3":
@@ -90,14 +90,17 @@ public class Driver {
 				
 			case "8":
 				// Code for reporting on balking
+				System.out.println(events.getBalkSummary());
 				break;
 				
 			case "9":
 				// Code for reporting on Reneging
+				System.out.println(events.getRenegeSummary());
 				break;
 				
 			case "10":
 				// Code for reporting on jockeying
+				System.out.println(events.getJockeySummary());
 				break;
 				
 			case "11":
@@ -117,15 +120,27 @@ public class Driver {
 	}
 
 
+	// Utility Functions
 	public static float giveRandomChance() {
+		// To be replaced with in-house code.
 		return (rng.nextFloat());
 	}
 	
-	public float giveRandomTime() {
-		return (rng.nextInt(6) + rng.nextFloat());
+	public static float giveRandomTimeLong() {
+		// Should output something between 1.0 - 2.5.
+		// To be replaced with in-house code.
+		return (rng.nextInt(2) + rng.nextFloat());
 	}
 	
-	public void createCustomer() throws IOException {
+	public static float giveRandomTimeShort() {
+		// Should output something between 0.0 - 1.0
+		// To be replaced with in-house code.
+		return (rng.nextFloat());
+	}
+	
+	
+	// Main Methods
+	public static void createCustomer() throws IOException {
 		System.out.print("Customer Name: ");
 		inputStr = input.readLine();
 		
@@ -135,15 +150,56 @@ public class Driver {
 		if (numServers == 1) {
 			if ((server1.getSize() > balkThreshold) && (rng.nextInt(2) == 1)) { // Balk calculation
 				// Log balk
+				System.out.println("Customer " + inputStr + " has balked with " + inputVal + " items.");
 				
+				// Add to log list. WIP
 			}
 			else {
 				// Customer enters queue despite being greater than threshold. 50/50 chance.
-				server1.enqueue(new Customer(inputStr, Integer.parseInt(inputVal)));
+				if (server1.getSize() == 0) {
+					totalTime += giveRandomTimeLong(); // Simulating the fact that time passes between batches of customers
+				}
+				server1.enqueue(new Customer(inputStr, Integer.parseInt(inputVal), totalTime));
 			}
 		}
 		else {
 			// When 2 servers we need to check some things.
+		}
+	}
+	
+	public static void processSingleCustomer() {
+		Customer temp;
+		if (server1.peek() != null) {
+			temp = server1.dequeue();
+			if (server2.getSize() == 0 && server1.getSize() != 0) {
+				// Jockey if server2 has noone in line and there's other people in your line.
+				server2.enqueue(temp);
+				
+			}
+		}
+		else {
+			System.out.println("The queue is empty. No customers to process.\n");
+		}
+	}
+	
+	public static void openServer2() {
+		if (numServers == 2) {
+			System.out.println("Second server is already opened!");
+		}
+		else {
+			numServers += 1; // SHOULD equal 2. 
+		}
+	}
+	
+	public static void closeServer2() {
+		if (numServers == 2) {
+			numServers -= 1; // Should close one server and go back to one.
+			
+			// Code for dequeueing entire server and entering them into new server.
+			// CHECK FOR BALK
+		}
+		else {
+			System.out.println("Second server is already closed!");
 		}
 	}
 }
